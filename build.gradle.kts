e7d62c1307f790a6a79f49d24789d94346ac803d
Duncan Calvert <dcalvert@ihmc.us>
2020-04-17 00:54:56 -0500

    Upgrade Gradle scripts to KTS

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.20.1"
+   id("us.ihmc.ihmc-ci") version "5.3"
+   id("us.ihmc.ihmc-cd") version "1.8"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.3.3"
+   vcsUrl = "https://github.com/ihmcrobotics/ihmc-java-decklink-capture"
+   openSource = true
+
+   configureDependencyResolution()
+   javaDirectory("main", "../../src")
+   resourceDirectory("main", "../../resources")
+   configurePublications()
+}
+
+app.entrypoint("Stream", "us.ihmc.javadecklink.Stream")
+
+mainDependencies {
+   api("us.ihmc:ihmc-native-library-loader:1.2.1")
+   api("com.martiansoftware:jsap:2.1")
+}
+
+val captureDirectory = "/home/shadylady/IHMCJavaDeckLinkCapture"
+
+tasks.create("deploy")
+{
+   dependsOn("installDist")
+
+   doLast {
+      remote.session("logger", "shadylady")
+      {
+         exec("mkdir -p $captureDirectory")
+
+         exec("rm -rf $captureDirectory/bin")
+         exec("rm -rf $captureDirectory/lib")
+
+         put(file("build/install/ihmc-java-decklink-capture/bin").toString(), "$captureDirectory/bin")
+         put(file("build/install/ihmc-java-decklink-capture/lib").toString(), "$captureDirectory/lib")
+
+         exec("chmod +x $captureDirectory/bin/Stream")
+      }
+   }
+}
