141d80053e6d970913f027ad5efdd8109006c7cf
Sylvain Bertrand <sbertrand@ihmc.us>
2021-10-26 13:29:19 -0500

    :bookmark: 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.3.4"
+   version = "0.4.0"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-java-decklink-capture"
    openSource = true
 
