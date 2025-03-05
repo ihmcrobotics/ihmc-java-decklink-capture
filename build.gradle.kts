9eda9f5ff2a7fc940140d2593902e58e5f1221f6
Dexton Anderson <danderson@ihmc.org>
2025-03-05 11:34:21 -0600

    :arrow_up: ihmc-native-library-loader 2.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ ihmc {
 app.entrypoint("Stream", "us.ihmc.javadecklink.Stream")
 
 mainDependencies {
-   api("us.ihmc:ihmc-native-library-loader:2.0.3")
+   api("us.ihmc:ihmc-native-library-loader:2.0.4")
    api("com.martiansoftware:jsap:2.1")
 }
 
